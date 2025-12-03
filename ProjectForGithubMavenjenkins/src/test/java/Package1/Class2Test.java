dc0d251a8b379d693e91c0f96caae1b074306abc
ankit <ankit@Ankitha>
2025-12-03 17:31:46 +0530

    My First Commit

---
+++ ProjectForGithubMavenjenkins/src/test/java/Package1/Class2Test.java
@@ -0,0 +1,16 @@
+package Package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class2Test {
+	
+	@Test
+	public void class1() {
+		Reporter.log("---------------Hiii----------------",true);
+
+	}
+
+	}
+
+
---
+++ ProjectForGithubMavenjenkins/src/test/java/Package1/Class3Test.java
@@ -0,0 +1,16 @@
+package Package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3Test {
+	
+	@Test
+	public void class1() {
+		Reporter.log("---------------Hiii----------------",true);
+
+	}
+
+	}
+
+
